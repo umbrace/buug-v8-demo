8edcb98e0f68b4b8e6f773b5cf81e8bb1d9f75c2
Damiaan <damiaan@comm-it.be>
2019-02-26 16:47:53 +0100

    Initial commit

---
+++ v8Demo/App_Plugins/MyContentApp/mycontentapp.controller.js
@@ -0,0 +1,13 @@
+﻿
+angular.module("umbraco")
+    .controller("My.MyContentApp", function ($scope, editorState, userService) {
+
+        var vm = this;
+        vm.CurrentNodeId = editorState.current.id;
+        vm.CurrentNodeAlias = editorState.current.contentTypeAlias;
+
+        var user = userService.getCurrentUser().then(function (user) {
+            console.log(user);
+            vm.UserName = user.name;
+        });
+    });
